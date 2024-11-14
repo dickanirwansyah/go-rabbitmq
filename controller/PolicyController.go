d36a51db95002184d6e93e2cdf6937d316ae7d39
muhamad dicka nirwansyah <dickanirwansyah@muhamads-Air.lan>
2024-11-14 21:56:03 +0700

    add docker rabbit mq and create update delete routes and controller

--- controller/PolicyController.go
+++ controller/PolicyController.go
@@ -7,9 +7,11 @@ import (
 	"producer/model"
 	"producer/rabbitmq"
 	"producer/util"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func InsertPolicy(c *fiber.Ctx) error {
@@ -41,4 +43,74 @@ func InsertPolicy(c *fiber.Ctx) error {
 	}
 
 	return util.SendResponse(c, "Success", policy, fiber.StatusCreated)
+}
+
+func UpdatePolicy(c *fiber.Ctx) error {
+
+	id, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid Policy ID !")
+	}
+
+	policy := model.Policy{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&policy); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid Request Body !")
+	}
+
+	if result := database.DB.Model(&policy).Updates(policy); result.Error != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed Update Data !")
+	}
+
+	return util.SendResponse(c, "Success", policy, fiber.StatusOK)
+}
+
+func GetPolicy(c *fiber.Ctx) error {
+
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid Policy ID !")
+	}
+
+	var policy model.Policy
+
+	result := database.DB.First(&policy, id)
+
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return fiber.NewError(fiber.StatusNotFound, "Policy By ID Notfound !")
+		}
+		return fiber.NewError(fiber.StatusInternalServerError, result.Error.Error())
+	}
+
+	return util.SendResponse(c, "success", policy, fiber.StatusAccepted)
+}
+
+func DeletePolicy(c *fiber.Ctx) error {
+
+	id, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid Policy ID !")
+	}
+
+	var policy model.Policy
+
+	result := database.DB.First(&policy, id)
+
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return fiber.NewError(fiber.StatusNotFound, "Policy By ID Notfound !")
+		}
+		return fiber.NewError(fiber.StatusInternalServerError, result.Error.Error())
+	}
+
+	if result := database.DB.Delete(&policy); result.Error != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, result.Error.Error())
+	}
+
+	return util.SendResponse(c, "success", id, fiber.StatusAccepted)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,8 @@ import (
 func SetupRoutes(app *fiber.App) {
 
 	app.Post("/api/v1/policy/insert", controller.InsertPolicy)
+	app.Get("/api/v1/policy/find/:id", controller.GetPolicy)
+	app.Put("/api/v1/policy/update/:id", controller.UpdatePolicy)
+	app.Delete("/api/v1/policy/delete/:id", controller.DeletePolicy)
 
 }
